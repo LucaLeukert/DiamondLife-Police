ea7e573bb23931b2adce5a747e4501b3e89f24bd
Luca Leukert <derluleu@gmail.com>
2022-03-10 21:35:00 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -1,84 +1,77 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
-import StatusCodes from 'http-status-codes';
-import express, { NextFunction, Request, Response } from 'express';
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import path from 'path'
+import helmet from 'helmet'
+import StatusCodes from 'http-status-codes'
+import express, { NextFunction, Request, Response } from 'express'
+import { engine } from 'express-handlebars'
 
-import 'express-async-errors';
+import 'express-async-errors'
 
-import BaseRouter from './routes/api';
-import logger from 'jet-logger';
-import { cookieProps } from '@routes/auth-router';
-import { CustomError } from '@shared/errors';
+import BaseRouter from './routes/api'
+import logger from 'jet-logger'
+import { cookieProps } from '@routes/auth-router'
+import { CustomError } from '@shared/errors'
 
-const app = express();
+const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser(cookieProps.secret))
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.engine('hbs', engine({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
 
-/************************************************************************************
- *                              Set basic express settings
- ***********************************************************************************/
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser(cookieProps.secret));
-
-// Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+	app.use(morgan('dev'))
 }
 
-// Security
 if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
+	app.use(helmet())
 }
 
-// Add APIs
-app.use('/api', BaseRouter);
+app.use('/api', BaseRouter)
 
-// Error handling
-app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
-    logger.err(err, true);
-    const status = (err instanceof CustomError ? err.HttpStatus : StatusCodes.BAD_REQUEST);
-    return res.status(status).json({
-        error: err.message,
-    });
-});
+app.use(
+	(err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
+		logger.err(err, true)
+		const status =
+			err instanceof CustomError
+				? err.HttpStatus
+				: StatusCodes.BAD_REQUEST
+		return res.status(status).json({
+			error: err.message,
+		})
+	}
+)
 
+const viewsDir = path.join(__dirname, 'views')
+app.set('views.hbs', viewsDir)
 
+const staticDir = path.join(__dirname, 'public')
+app.use(express.static(staticDir))
 
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-// Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-
-// Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-// Nav to login pg by default
 app.get('/', (_: Request, res: Response) => {
-    res.sendFile('login.html', {root: viewsDir});
-});
+	res.render('homepage')
+})
 
-// Redirect to login if not logged in.
+app.get('/faq', (_: Request, res: Response) => {
+	res.render('faq')
+})
+
+app.get('/question', (_: Request, res: Response) => {
+	res.render('question')
+})
+
 app.get('/users', (req: Request, res: Response) => {
-    const jwt = req.signedCookies[cookieProps.key];
-    if (!jwt) {
-        res.redirect('/');
-    } else {
-        res.sendFile('users.html', {root: viewsDir});
-    }
-});
+	const jwt = req.signedCookies[cookieProps.key]
+	if (!jwt) {
+		res.redirect('/')
+	} else {
+		res.sendFile('users.html', { root: viewsDir })
+	}
+})
 
-
-
-/************************************************************************************
- *                              Export Server
- ***********************************************************************************/
-
-export default app;
+export default app
