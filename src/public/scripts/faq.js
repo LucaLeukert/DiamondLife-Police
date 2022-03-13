a58a5bcd3d14c9dd108c1e9f058ad03105c9c9bb
Luca Leukert <derluleu@gmail.com>
2022-03-13 20:47:09 +0100

    Update faq.js

--- src/public/scripts/faq.js
+++ src/public/scripts/faq.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
 	$('#list--search').on('keyup', function () {
 		var value = $(this).val().toLowerCase()
+
+		if (value === 'yung hurn') {
+			window.location = 'https://www.youtube.com/watch?v=sVjf-NlHpHQ'
+		}
+
 		$('#list li').filter(function () {
 			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
 		})
