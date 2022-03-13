b922524c4d91c7ea61e9b5aaf0c0c7b11cb0de92
Luca Leukert <derluleu@gmail.com>
2022-03-13 19:37:59 +0100

    Delete src directory

--- src/public/scripts/faq.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-	$('#list--search').on('keyup', function () {
-		var value = $(this).val().toLowerCase()
-		$('#list li').filter(function () {
-			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-		})
-	})
-})
